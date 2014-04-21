93f6070dd979a3818ba0a6dea82ade2a08f5dd7e
Anurag Vaishwade <ranurag@zeomega.com>
2014-04-21 12:29:48 +0530

    Answer 1

---
+++ Assessment.py
@@ -0,0 +1,11 @@
+''' 1.) It is possible to do multiple assignments at once ? Give an example of one ?
+	Ans.) Yes '''
+
+def mul_assignment():
+	'''function for multiple assignments'''
+	a,b,c = 1,2,3
+	print a,b,c
+
+
+if __name__ == '__main__':
+	mul_assignment()
