3c988829bf5c644eca04221c1774fc9b7ae10a98
Anurag Vaishwade <ranurag@zeomega.com>
2014-04-21 12:35:47 +0530

    Answer 2

--- Assessment.py
+++ Assessment.py
@@ -4,8 +4,21 @@
 def mul_assignment():
 	'''function for multiple assignments'''
 	a,b,c = 1,2,3
-	print a,b,c
+	print "problem 1 =>",a,b,c
+
+'''2.) Write simple program to swapping values of 2 variables ?'''
+
+def swapping_value(a,b):
+	'''function for swapping values'''
+	print "problem 2 =>"
+	print "Before swapping=>" , a,b
+	a,b = b,a
+	print "After swapping=>" , a,b
+
+def main():
+	mul_assignment()
+	swapping_value(4,5)
 
 
 if __name__ == '__main__':
-	mul_assignment()
+	main()
